aba0b236cb26be2d6bd5d57f2b3fa74a16fa031b
Matt Craddock <5796417+craddm@users.noreply.github.com>
2024-04-03 12:17:43 +0100

    Slightly improve documentation

--- implementation/matmul-r/matmul.R
+++ implementation/matmul-r/matmul.R
@@ -1,12 +1,16 @@
 # I use the `reticulate` package to interface with Python/Numpy and load the
 # test data. See
 # https://rstudio.github.io/reticulate/articles/python_packages.html for more
-# details on how to get that running I use the `bench` package to actually run
-# benchmarking, rather than manually running and timing loops etc.
+# details on how to get that running. I also use the `bench` package to actually
+# run benchmarking, rather than manually running and timing loops etc. This has
+# the advantage of, by default, removing iterations where garbage collection
+# took place.
 
-# Uncomment the following line to install the packages, but you will then also want
-# to set up `reticulate` as per the instructions linked above.
-# install.packages(c("reticulate", "bench"))
+# Uncomment the following line to install the necessary packages. You will then
+# also want to set up `reticulate` as per the instructions linked above, making
+# sure to install numpy.
+
+#install.packages(c("reticulate", "bench"))
 
 library(reticulate)
 use_miniconda("r-reticulate")
@@ -40,6 +44,9 @@ multiply_matrices <- function(a_mats, b_mats) {
     function(x) a_mats[[x]] %*% b_mats[[x]]
   )
 }
+
+# If you want to include runs where garbage collection took place, pass the
+# argument `filter_gc = TRUE`)
 
 bench_results <- bench::mark(
   multiply_matrices(a_matrices,
