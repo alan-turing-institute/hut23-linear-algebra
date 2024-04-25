8efc9cfe4733ee724795615dba8a18b264637fc9
David Llewellyn-Jones <dllewellyn-jones@turing.ac.uk>
2024-04-25 22:26:18 +0100

    Add large matrix benchmarks, refactor tests
    Refeactos the codebase to split the tests and benchmarks into separate
    files. Adds some large matrix multiplication benchmarks for the serial
    and parallel implementations.

---
+++ implementation/matmul-c/include/benchmarks.h
@@ -0,0 +1,24 @@
+/* vim: noet:ts=2:sts=2:sw=2 */ 
+
+/* SPDX-License-Identifier: MIT */
+/* Copyright © 2024 David Llewellyn-Jones */
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#ifndef __MATRIX_BENCHMARKS_H
+#define __MATRIX_BENCHMARKS_H (1)
+
+typedef struct _Benchmark Benchmark;
+
+Benchmark * new_benchmark();
+Benchmark * delete_benchmark(Benchmark *benchmark);
+
+void benchmarks_start(Benchmark *benchmark, uint32_t operations);
+void benchmarks_end(Benchmark *benchmark);
+void benchmark_set_quiet(Benchmark *benchmark, bool quiet);
+void benchmarks_multiply_big(ThreadPool *pool);
+void benchmarks_multiply_small(Matrices *a, Matrices *b, Matrices *d);
+
+#endif /* __MATRIX_BENCHMARKS_H */
+
