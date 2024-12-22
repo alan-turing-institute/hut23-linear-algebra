c74792c12072110c0c71d19d7785e40ed234b1c8
David Llewellyn-Jones <dllewellyn-jones@turing.ac.uk>
2024-12-22 11:44:22 +0000

    Add initial oneMKL implementation
    Based on the threaded C implementation, so this is starting from a
    direct copy the matmul-c-threaded directory, just with filenames changed
    and the vim headers fixed.

---
+++ implementation/matmul-cpp-mkl/include/utils.hpp
@@ -0,0 +1,19 @@
+/* vim: set noet ts=2 sts=2 sw=2: */
+
+/* SPDX-License-Identifier: MIT */
+/* Copyright © 2024 David Llewellyn-Jones */
+
+#ifndef __MATRIX_UTILS_H
+#define __MATRIX_UTILS_H (1)
+
+#include <stdint.h>
+
+typedef struct _Rand Rand;
+
+Rand * new_rand();
+Rand * delete_rand(Rand *rand);
+void rand_seed(Rand *rand, uint32_t seed);
+double rand_next(Rand *rand);
+double rand_value(Rand *rand);
+
+#endif /* __MATRIX_UTILS_H */
---
+++ implementation/matmul-cpp-mkl/src/tests.cpp
@@ -0,0 +1,112 @@
+/* vim: set noet ts=2 sts=2 sw=2: */
+
+/* SPDX-License-Identifier: MIT */
+/* Copyright © 2024 David Llewellyn-Jones */
+
+#include <stdlib.h>
+
+#include "operations.hpp"
+
+#include "tests.hpp"
+
+uint32_t tests_load_matrices(Matrices *a, Matrices *b, Matrices *c) {
+	uint32_t total = 0;
+
+	printf("Reading test matrices from file\n");	
+
+	Matrices *matrices = new_matrices(0);
+	matrix_npz_load("../testdata/matrices.npz", matrices);
+	if ((matrices->count % 3) == 0) {
+		total = matrices->count / 3;
+	}
+	else {
+		printf("Test matrices must come in A, B, C triplets\n");
+	}
+
+	a->matrices = realloc(a->matrices, sizeof(NamedMatrix) * total);
+	a->count = total;
+	b->matrices = realloc(b->matrices, sizeof(NamedMatrix) * total);
+	b->count = total;
+	c->matrices = realloc(c->matrices, sizeof(NamedMatrix) * total);
+	c->count = total;
+
+	NamedMatrix *from;
+	NamedMatrix *to;
+	for (uint32_t index = 0; index < matrices->count; ++index) {
+		from = &matrices->matrices[index];
+		to = NULL;
+		int read = 0;
+		uint32_t count = 0;
+		read = sscanf(from->name, "a_%d.npy", &count);
+		if ((read == 1) && (count < total)) {
+			to = &a->matrices[count];
+		}
+		read = sscanf(from->name, "b_%d.npy", &count);
+		if ((read == 1) && (count < total)) {
+			to = &b->matrices[count];
+		}
+		read = sscanf(from->name, "c_%d.npy", &count);
+		if ((read == 1) && (count < total)) {
+			to = &c->matrices[count];
+		}
+		if (to) {
+			to->name = from->name;
+			from->name = NULL;
+			to->matrix = from->matrix;
+			from->matrix = NULL;
+		}
+	}
+
+	matrices = delete_matrices(matrices);
+
+	for (uint32_t index = 0; index < total; ++index) {
+		if (!a->matrices[index].matrix || !b->matrices[index].matrix || !c->matrices[index].matrix) {
+			printf("Missing matrix: %u\n", index);
+		}
+	}
+
+	return total;
+}
+
+bool tests_allocate_results(Matrices *c, Matrices *d) {
+	for (uint32_t index = 0; index < c->count; ++index) {
+		d->matrices[index].matrix = new_matrix(c->matrices[index].matrix->height, c->matrices[index].matrix->width);
+	}
+	return true;
+}
+
+// Perform 512 multiplications and compare against the results from NumPy
+void tests_compare(Matrices *a, Matrices *b, Matrices *c, Matrices *d, ThreadPool *pool) {
+	printf("Performing unit tests...\n");
+	uint32_t passed = 0;
+	uint32_t total = d->count;
+	bool result;
+
+	for (uint32_t index = 0; index < total; ++index) {
+		result = multiply(d->matrices[index].matrix, a->matrices[index].matrix, b->matrices[index].matrix);
+		result = result && equals(c->matrices[index].matrix, d->matrices[index].matrix);
+		if (result) {
+			passed += 1;
+		}
+		else {
+			printf("Incorrect result\n");
+			matrix_print(c->matrices[index].matrix);
+			matrix_print(d->matrices[index].matrix);
+		}
+	}
+	for (uint32_t index = 0; index < total; ++index) {
+		result = multiply_parallel(pool, d->matrices[index].matrix, a->matrices[index].matrix, b->matrices[index].matrix);
+		result = result && equals(c->matrices[index].matrix, d->matrices[index].matrix);
+		if (result) {
+			passed += 1;
+		}
+		else {
+			printf("Incorrect result\n");
+			matrix_print(c->matrices[index].matrix);
+			matrix_print(d->matrices[index].matrix);
+		}
+	}
+	printf("Multiplication tests passed: %u out of %u\n", passed, total * 2);
+}
+
+
